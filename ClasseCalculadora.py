a4ac4e39fce6aeaa3d9eab0b485fcb7fb4658bde
tiagorc <tiagobrasil2005@yahoo.com.br>
2016-04-13 16:04:48 -0300

    Merge request in branch subtracao

--- ClasseCalculadora.py
+++ ClasseCalculadora.py
@@ -1,14 +1,18 @@
 class ClasseCalculadora():
 
-        def multiplicacao(self, x, y) 
-        	return x*y
-
+	def multiplicacao(self, x, y):
+		return x * y
 
 	def divisao(self,x,y):
 		return x/y
 
-		
+	def subtracao(self,x,y):
+		return x - y
+
+
 if __name__=='__main__':
 	calculadora = ClasseCalculadora()
 
-	calculadora.divisao(10,2)
+	print calculadora.divisao(10,2)
+	print calculadora.multiplicacao(1,2)
+	print calculadora.subtracao(3,1)
